0431c5171650f944fa397c678efe84959aff3f61
Matvey Voropay <alexston228@gmail.com>
2022-10-17 23:47:18 +0300

    add forms

--- Vihlop/Vihlop/Form1.cs
+++ Vihlop/Vihlop/Form1.cs
@@ -15,7 +15,30 @@ namespace Vihlop
         public Form1()
         {
             InitializeComponent();
-            aboba
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 form3 = new Form3();
+            form3.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form4 form4 = new Form4();
+            form4.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
